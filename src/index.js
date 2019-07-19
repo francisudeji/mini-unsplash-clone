05b823b1428ab47b155983ca033c024e90de0ffb
francisudeji <francis.udejiofficial@gmail.com>
2019-07-19 19:11:06 +0100

    Finished design and fetching of photos

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import Unsplash from 'unsplash-js'
-
-export const unsplash = new Unsplash({
-  applicationId: process.env.REACT_APP_APP_ID,
-  secret: process.env.REACT_APP_APP_SECRET
-})
 
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
